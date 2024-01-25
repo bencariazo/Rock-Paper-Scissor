5d2967d72d73d310db840908303534b6f5def577
bencariazo <30350225+bencariazo@users.noreply.github.com>
2024-01-25 14:14:57 +0800

    Add playRound function
    - accepts two arguments from player and computer
    - checks arguments and returs a starting

--- rps.js
+++ rps.js
@@ -3,5 +3,34 @@ function getComputerChoice(){
     const item = ["rock","paper","scissors"]
     let chooseItem = Math.floor(Math.random() * item.length)
     return item[chooseItem]
-}
+};
 
+// create a function that would play the game
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == "rock"){
+        if(computerSelection == "paper"){
+            return "You Lose! Paper beats Rock!"
+        } else {
+            return "You Win! Scissors can't cut Rock!"
+        }
+    } else if (playerSelection == "paper"){
+        if(computerSelection == "scissors"){
+            return "You Lose! Scissors cuts Paper!"
+        } else {
+            return "You Win! Paper beats Rock!"
+        }
+    } else if (playerSelection == "scissors"){
+        if(computerSelection == "paper"){
+            return "You Win! Scissors cuts Paper"
+        } else {
+            return "You Lose! Rock beat Scissors!"
+        }
+    }
+};
+
+const playerSelection = prompt("What would you choose (Rock, Paper, or Scissors)?")
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+console.log(playerSelection)
+
+console.log(playRound(playerSelection.toLowerCase(),computerSelection))
