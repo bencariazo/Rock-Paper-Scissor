012f12af6ac8887455fc49881bed15d257d499ff
bencariazo <30350225+bencariazo@users.noreply.github.com>
2024-03-21 09:54:50 +0800

    feat: add getComputerChoice function
    - declare array item to iterate from
    - declare variable for and get value from array randomly

--- rps.js
+++ rps.js
@@ -80,6 +80,14 @@ function playRound(playerSelection, computerSelection){
         }
     }
 };
+
+// create a function that would choose random item for the computer
+function getComputerChoice(){
+    const item = ["rock","paper","scissors"]
+    let chooseItem = Math.floor(Math.random() * item.length)
+    return item[chooseItem]
+};
+
     const computerSelection = getComputerChoice();
     // console.log(computerSelection)
     playRound(playerSelection.toLowerCase(),computerSelection)
